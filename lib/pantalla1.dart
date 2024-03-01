1c0fdf807a811dab590878e8178f8cb54a2b1b68
GarciaJ128 <143743623+GarciaJ128@users.noreply.github.com>
2024-03-01 13:20:49 -0700

    Update pantalla1.dart

--- lib/pantalla1.dart
+++ lib/pantalla1.dart
@@ -10,14 +10,38 @@ class Pantalla1 extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pantalla 1 Jennifer Garcia'),
       ),
-      body: Center(
-          child: ElevatedButton(
-        child: const Text("Vamos a la pantalla 2"),
-        onPressed: () {
-          Navigator.pushNamed(context, "/pantalla2",
-              arguments: "Mensa desde la pantalla1");
-        },
-      )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Text("FlutLab is on your service!",
+              style: TextStyle(fontSize: 30, color: Colors.indigo)),
+          Icon(
+            Icons.mood,
+            size: 50,
+            color: Colors.deepPurple,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+            child: const Text(
+              "Vamos a la pantalla 2",
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, "/pantalla2",
+                  arguments: "Mensa desde la pantalla1");
+            },
+          ),
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                "Tarjeta GarciaJ",
+                style: TextStyle(fontSize: 30, color: Colors.indigo),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   } //Widget
 } //Clase pantalla 1
